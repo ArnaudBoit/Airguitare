cd1f99fb5518f675395e283becbbfdde11dafa01
Stagiaire <Stagiaire@AFCEPF-1RE7PR91>
2016-12-19 12:33:42 +0100

    zd

--- Airguitare_Web/WebContent/script/singleArticle.js
+++ Airguitare_Web/WebContent/script/singleArticle.js
@@ -26,14 +26,15 @@ app.controller('BUarticle', function ($scope, $http, $window) {
 		$scope.addOne = function myfunction(produit){
 	    	var nbArticles ;
 			if("nbArt" in localStorage){
+				console.log("panier pas vide");
 			    nbArticles = parseInt(localStorage.getItem('nbArt')); 
 			    nbArticles += parseInt($scope.nbProduit);
 			    var panier = document.getElementById('recapPanier').innerHTML;
-			     var add = $scope.article.prix;
-//			     console.log("produit: "+add+" - nbArticlesAjout: "+$scope.nbProduit);
+			     produit = $scope.article;
+			     console.log("produit: "+produit+" - nbArticlesAjout: "+$scope.nbProduit);
 //			     console.log("panier: "+panier+" - nbArticlesPanier: "+nbArticles);
 //			     console.log("ajoutPrix: "+add*$scope.nbProduit);
-			     var somme= parseInt(panier) + add*$scope.nbProduit;
+			     var somme= parseInt(panier) + produit.prix*$scope.nbProduit;
 			     document.getElementById('recapPanier').innerHTML = somme;
 			     localStorage.setItem('cost',somme);
 			    
@@ -42,9 +43,13 @@ app.controller('BUarticle', function ($scope, $http, $window) {
 			} else {
 			   localStorage.setItem('nbArt', $scope.nbProduit);
 			   nbArticles =  $scope.nbProduit; 
+			   var somme = produit.prix*$scope.nbProduit;
+				console.log("panier vide, produit prix = "+somme);
+			     document.getElementById('recapPanier').innerHTML = somme;
+			     localStorage.setItem('cost', somme);
 			}
 			document.getElementById('nbArticle').innerHTML = nbArticles + "" + (nbArticles <=1? " article" : " articles");
-			$scope.saveItem(add, $scope.nbProduit);
+			$scope.saveItem(produit, $scope.nbProduit);
 		}
 		
     	$scope.saveItem = function myfunction(produit, qte){
